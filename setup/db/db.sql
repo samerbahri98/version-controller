f965c6e6414d4533175b85f13ad7e2b49ec3d4c6
samer.bahi98 <samer.bahi@ieee.org>
2021-10-29 02:21:32 +0200

    feat: database

--- setup/db/db.sql
+++ setup/db/db.sql
@@ -10,41 +10,60 @@ BEGIN;
         user_id uuid DEFAULT uuid_generate_v4 (),
         first_name TEXT NOT NULL,
         last_name TEXT NOT NULL,
-        nickname TEXT NOT NULL,
-        email email  NOT NULL,
+        username TEXT UNIQUE NOT NULL,
+        email email UNIQUE NOT NULL,
         password TEXT NOT NULL,
         phone TEXT,
+        created_at TIMESTAMP DEFAULT current_timestamp,
+        lock INTEGER DEFAULT 0,
         PRIMARY KEY (user_id)
     );
 
 
-    CREATE OR REPLACE FUNCTION validate_user(_email email, _password TEXT) RETURNS uuid
-    LANGUAGE SQL
+    CREATE OR REPLACE FUNCTION validate_user(_email email, _password TEXT) RETURNS TABLE (userid uuid,first_name TEXT,last_name TEXT,username TEXT,phone TEXT, lock INTEGER )
+    LANGUAGE plpgsql
     AS $$
-        DECLARE _id uuid;
-        counter INTEGER;
-
+        DECLARE
+            counter INTEGER;
         BEGIN
-            SELECT COUNT(*) INTO counter,user_id INTO _id
+            CREATE TEMP TABLE tempuser (
+                   user_id uuid,
+                   first_name TEXT,
+                   last_name TEXT,
+                   username TEXT,
+                   phone TEXT,
+                   lock INTEGER
+            );
+            INSERT INTO tempuser
+            SELECT user_data.user_id,user_data.first_name,user_data.last_name,user_data.username,user_data.phone,user_data.lock
             FROM user_data
             WHERE email=_email AND password = crypt(_password,password);
+            SELECT COUNT(*) INTO COUNTER FROM tempuser;
             IF counter=0 THEN
                 RAISE EXCEPTION 'USER DOESNT EXIST';
-            ELSE
-                RETURN id;
             END IF;
+            RETURN QUERY SELECT * FROM tempuser;
+
         END
     $$ ;
 
 
-    CREATE OR REPLACE PROCEDURE create_user(_first_name TEXT,_last_name TEXT, _nickname TEXT, _email email, _password TEXT, _phone TEXT)
+    CREATE OR REPLACE PROCEDURE create_user(_first_name TEXT,_last_name TEXT, _username TEXT, _email email, _password TEXT, _phone TEXT)
     LANGUAGE SQL
     AS $$
-        INSERT INTO user_data(first_name,last_name,nickname, email,password,phone)
-        VALUES (_first_name,_last_name,_nickname,_email,crypt(_password,gen_salt('bf')),_phone)
+        INSERT INTO user_data(first_name,last_name,username, email,password,phone)
+        VALUES (_first_name,_last_name,_username,_email,crypt(_password,gen_salt('bf')),_phone)
     $$;
 
 
 
+    CREATE TABLE repositories(
+        repository_id uuid DEFAULT uuid_generate_v4 (),
+        repository_name TEXT NOT NULL,
+        created_by uuid NOT NULL,
+        created_at TIMESTAMP DEFAULT current_timestamp,
+        CONSTRAINT created_by_id_ FOREIGN KEY(created_by) REFERENCES user_data(user_id),
+        PRIMARY KEY (repository_id)
+    );
 END TRANSACTION;
 COMMIT;
