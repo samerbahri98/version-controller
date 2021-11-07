0e037597226ea27b5172a1fc95968175743cc34b
samer.bahi98 <samer.bahi@ieee.org>
2021-11-07 08:13:57 +0100

    feat: working graphql server setup

---
+++ api/src/index.ts
@@ -0,0 +1,38 @@
+// DEPENDENCIES
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import {
+	ApolloServerPluginLandingPageGraphQLPlayground,
+	ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core";
+import cors from "cors";
+import helmet from "helmet";
+import { buildSchema } from "type-graphql";
+import { RegisterResolver } from "./graphql/User/Register";
+const morgan = require("morgan");
+
+(async () => {
+	const app = express();
+
+	// MIDDLEWARES
+	app.use(cors());
+	app.use(helmet({ contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false }));
+	app.use(morgan("dev"));
+
+	// GRAPHQL
+	const schema = await buildSchema({
+		resolvers: [RegisterResolver],
+	});
+	const apolloServer = new ApolloServer({
+		schema,
+		plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+	});
+
+	await apolloServer.start();
+
+	apolloServer.applyMiddleware({ app });
+
+	// SERVER
+	const PORT = process.env.PORT || 3300;
+	app.listen(PORT, () => console.log("server online"));
+})();
---
+++ api/src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+	id?: string;
+	firstName?: string;
+	lastName?: string;
+	userName?: string;
+	email?: string;
+    password?: string;
+	createdAt?: Date;
+	attributionTag?: string;
+	phone?: string;
+}
