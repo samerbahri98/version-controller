cdde885706179adf5a0c0ffa5bef83c6f1a87785
samer.bahi98 <samer.bahi@ieee.org>
2021-11-30 20:59:30 +0100

    feat: restart safety

--- api/src/config/ssh.ts
+++ api/src/config/ssh.ts
@@ -5,8 +5,21 @@ import { generateKeyPair } from "crypto";
 import * as sshpk from "sshpk";
 const { parseKey, parsePrivateKey } = sshpk;
 import * as path from "path";
+import { User } from "../entities/User";
+import { PublicKey } from "../entities/PublicKey";
 
 const privateKeyPath = path.join(__dirname, "id_rsa");
+
+const getPasswords: () => Promise<string[]> = async () => {
+	const users = await User.find({ select: ["username", "password"] });
+	const hashes = users.map((user: User) => `${user.username}:${user.password}`);
+	return hashes;
+};
+
+const getPublicKeys: () => Promise<string[]> = async () => {
+	const keys = await PublicKey.find({ select: ["public_key_hash"] });
+	return keys.map((key) => key.public_key_hash);
+};
 
 export const SshInit = (() => {
 	return new Promise(async (resolve, reject) => {
@@ -37,25 +50,39 @@ export const SshInit = (() => {
 						const parsedPrivateKey = sshpk.parsePrivateKey(privateKey, "pem");
 						const parsedPublicKey = parsedPrivateKey.toPublic().toString("ssh");
 						// Add authorized_keys
-
+						const passwords = await getPasswords();
+						const publicKeys = await getPublicKeys();
 						const commandSequence = [
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chmod 777 /etc/ssh/sshd_config`, // enable editing the sshd_config
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S echo 'PasswordAuthentication no\nUsePam no\nPermitRootLogin no\nPermitRootLogin prohibit-password' >> /etc/ssh/sshd_config`, //disable password auth
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chmod 644 /etc/ssh/sshd_config`, // disable editing the sshd_config
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S htpasswd -b -c '/var/git/.htpasswd' 'git' '${process.env.GIT_CONTAINER_PASSWORD}'`,
+							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chgrp -R 'git' '/var/git/.htpasswd'`,
+							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chown -R 'git' '/var/git/.htpasswd'`,
+							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chmod 777 -R '/var/git/.htpasswd'`,
+							...passwords.map(
+								(password) => `echo '${password}' >> /var/git/.htpasswd  `
+							),
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S mkdir '/var/git/.ssh'`,
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S touch '/var/git/.hushlogin'`,
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S touch '/var/git/.ssh/authorized_keys'`,
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chgrp -R 'git' '/var/git/.ssh'`,
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chown -R 'git' '/var/git/.ssh'`,
+							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S chmod 777 -R '/var/git/.ssh'`,
 							`echo '${parsedPublicKey}' >> /home/.ssh/authorized_keys`, // enable editing the sshd_config
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S /etc/init.d/apache2 restart`, //restart http
+
+							...publicKeys.map(
+								(key) => `echo '${key}' >> /var/git/.ssh/authorized_keys  `
+							),
 							`echo '${process.env.GIT_CONTAINER_PASSWORD}' | sudo -S /etc/init.d/ssh reload`, //restart ssh
 						];
 
 						for (const command of commandSequence) {
 							try {
-								await sshAuthNode.execCommand(command);
+								// console.log(command);
+								const res = await sshAuthNode.execCommand(command);
+								console.log({ command, res });
 							} catch (err) {
 								throw err;
 							}
